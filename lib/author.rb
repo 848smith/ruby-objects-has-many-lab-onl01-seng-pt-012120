6ae823bf3bc060dbb8937863a8ac7132cc83d854
Andrew Smith <848smith@gmail.com>
2020-02-19 04:17:45 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -4,4 +4,20 @@ class Author
   def initialize(name)
     @name = name
   end
+  
+  def posts
+    
+  end
+  
+  def add_post
+    
+  end
+  
+  def add_post_by_title
+    
+  end
+  
+  def self.post.count
+    
+  end
 end

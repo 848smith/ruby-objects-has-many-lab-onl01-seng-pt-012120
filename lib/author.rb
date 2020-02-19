43f5dc5b5fa65ee62ab7ca0417728b8c7456aeb4
Andrew Smith <848smith@gmail.com>
2020-02-19 04:20:31 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author
   attr_accessor :name
+  @@count = 0
   
   def initialize(name)
     @name = name
@@ -7,21 +8,22 @@ class Author
   end
   
   def posts
-    
+    @titles
   end
   
   def add_post(title)
     @titles << title
     post.author = self
-    
+    @@count += 1
   end
   
   def add_post_by_title(title)
-    
-    
+    post.author = self
+    @title << title
+    @@count += 1
   end
   
   def self.post_count
-    
+    @@count
   end
 end

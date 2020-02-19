9d6a64d8b78b305854c438a2bf184b1ac8daeac5
Andrew Smith <848smith@gmail.com>
2020-02-19 03:47:06 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,21 @@
 class Post
-  attr_accessor :name, :author
+  attr_accessor :title, :author
   @@all = []
   
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def author_name
+    if self.author
+      self.author.name
+    else
+      nil
+    end
   end
 end

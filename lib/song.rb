1fe3a27b01daca5bf8111ca932fe3107a83cb153
Andrew Smith <848smith@gmail.com>
2020-02-19 03:34:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,11 @@
 class Song
   attr_accessor :name, :artist
   
-  def initialize(name)
+  def initialize(name, title = 0)
     @name = name
     @@all = []
     @@all << self
+    @@all << title
   end
   
   def self.all
